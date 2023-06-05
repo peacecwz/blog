6341305b81a42383b5d76100e47894fc6f56a31f
Baris Ceviz <baris@ceviz.dev>
2023-06-05 19:37:50 +0200

    fix: image optimization

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-        turboMode: true,
-    },
     images: {
         domains: ['cdn-images-1.medium.com'],
     }

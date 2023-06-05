e6893b4d5ca480a65664ccd20cb471b0b5d14017
Baris Ceviz <baris@ceviz.dev>
2023-06-05 19:24:21 +0200

    feat: enable experimantel feature

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true,
+        turboMode: true,
+    },
     images: {
         domains: ['cdn-images-1.medium.com'],
     }

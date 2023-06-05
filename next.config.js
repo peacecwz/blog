b1f7713837acafbc45deab66c59d2495033d6395
Baris Ceviz <baris@ceviz.dev>
2023-06-05 19:15:15 +0200

    feat: add image optimization

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['cdn-images-1.medium.com'],
+    }
+}
+
 
 module.exports = nextConfig

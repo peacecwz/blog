d7c18430a732f3fb0aaf25bb372f730246fd73c4
Baris Ceviz <baris@ceviz.dev>
2023-07-19 23:49:38 +0200

    feat: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,33 @@
+export default function Custom404() {
+    return <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+    }}>
+        <p>
+            If you only knew the power
+        </p>
+        <p>
+            Of the server side.
+        </p>
+        <p>
+            Google never told you
+        </p>
+        <p>
+            What happened to your link.
+        </p>
+
+        <p>Client: It told me enough. It told me you removed it.</p>
+
+        <h1>I'm your 404 page...</h1>
+        <img style={{
+            margin: 'auto',
+            width: '100%',
+        }} src="/404.gif" alt="404"/>
+        <a href='https://www.youtube.com/watch?v=_lOT2p_FCvA' target='_blank' rel='noreferrer'>
+            <p>Search your feelings, you know it to be true.</p>
+        </a>
+    </div>
+}
